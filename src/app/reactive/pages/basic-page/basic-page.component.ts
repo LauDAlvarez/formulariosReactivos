f76f3583babb6c84e278d8fd6a18bba77f36c4f5
Lautaro D Alvarez <lautarodalvarez@gmail.com>
2024-03-30 19:57:15 -0300

    Update 30/3

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { FormBuilder,  Validators, FormGroup } from '@angular/forms';
 
+const rtx4070 = {
+  name: 'RTX4070',
+  price: 2000,
+  inStorage: 20
+}
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
 export class BasicPageComponent {
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // }) Manera de realizar un formulario reactivo
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(1)]],
+    inStorage: [0, [Validators.required, Validators.min(1)]],
+  })
+
+  constructor(
+    private fb: FormBuilder
+  ){}
+
+  isValidField(fieldName: string): boolean | null{
+    return this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched;   
+  }
+
+  getFieldError( fieldName: string): string | null{
+    if(!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required': 
+          return `Este campo es requerido`;
+        case 'minlength':
+          return `Ingresa al menos ${ errors['minlength'].requiredLength } caracteres`;
+      }
+    }
+
+    return '';
+  }
+
+  onSave(): void{
+    if( this.myForm.invalid ) return;
+
+    console.log(this.myForm.value);
+    
+    this.myForm.reset(rtx4070);
+  }
+  
 
 }
--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,75 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    favoriteGames:   this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ])
+  });
+
+  public newFavorite: FormControl = new FormControl('', Validators.required);
+
+  constructor( private fb: FormBuilder){}
+
+  get favoriteGamesControl(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField(fieldName: string): boolean | null{
+    return this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched;   
+  }
+
+  isValidFieldInArray(formArray: FormArray, i: number){
+    return formArray.controls[i].errors && formArray.controls[i].touched;
+  }
+
+  getFieldError( fieldName: string): string | null{
+    if(!this.myForm.controls[fieldName]) return null;
+
+    const errors = this.myForm.controls[fieldName].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch(key){
+        case 'required': 
+          return `Este campo es requerido`;
+        case 'minlength':
+          return `Ingresa al menos ${ errors['minlength'].requiredLength } caracteres`;
+      }
+    }
+
+    return '';
+  }
+
+  onDeleteFavorite(i: number): void {
+    this.favoriteGamesControl.removeAt(i);
+  }
+
+  onAddToFavorite(): void{
+    if( this.newFavorite.invalid ) return;
+
+    const newGame = this.newFavorite.value
+    this.favoriteGamesControl.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
+  }
+
+  onSubmit(): void{
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.myForm.controls['favoriteGames'] = new FormArray([])
+    this.myForm.reset()
+  }
 }
